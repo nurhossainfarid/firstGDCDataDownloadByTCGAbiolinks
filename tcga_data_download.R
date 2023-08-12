a36ba993c1dc0c5e5a5bd5e856ec63dae52142e9
nurhossainfarid <faahsan1516@gmail.com>
2023-08-12 14:29:41 +0600

    Start query to retrieve gene expression data

--- tcga_data_download.R
+++ tcga_data_download.R
@@ -1,7 +1,15 @@
-#
-
+# necessary library
 library(TCGAbiolinks)
 library(tidyverse)
 library(maftools)
 library(pheatmap)
 library(SummarizedExperiment)
+
+# get a list of projects
+gdcprojects <- getGDCprojects()
+getProjectSummary('TCGA-BRCA')
+
+#building a query
+query_TCGA <- GDCquery(project= 'TCGA-BRCA',
+                       data.category = 'Transcriptome Profiling')
+View(query_TCGA)
